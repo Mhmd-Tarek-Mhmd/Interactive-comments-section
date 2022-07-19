861cf78894db8564fab3bd1e0f933fab8909b745
Mhmd <biboo.biboo97@gmail.com>
2022-07-20 01:51:22 +0200

    EDIT: userImage styles

--- src/components/Comment/UserImage.jsx
+++ src/components/Comment/UserImage.jsx
@@ -1,6 +1,8 @@
+import { userImage } from "./comment.module.css";
+
 function UserImage({ user }) {
   return (
-    <picture>
+    <picture className={userImage}>
       <source srcSet={user.images.webp} type="image/webp" />
       <source srcSet={user.images.png} type="image/png" />
       <img src={user.images.png} alt={`Avatar of ${user.username}`} />
