995ad31049635c177b43d9c746022919431ac640
Mhmd <biboo.biboo97@gmail.com>
2022-07-20 03:45:08 +0200

    EDIT: add from

--- src/components/Forms/Add.jsx
+++ src/components/Forms/Add.jsx
@@ -5,15 +5,17 @@ import Context from "../../context";
 import UserImage from "../Comment/UserImage";
 import { addForm, comment, reply } from "./forms.module.css";
 
-function Add({ parent, user }) {
+function Add({ parent, toggleIsAddReply }) {
   const [content, setContent] = useState("");
-  const { comments } = useContext(Context);
+  const { comments, authedUser } = useContext(Context);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     parent
       ? comments.actions.addReply(parent.id, content, parent.user.username)
       : comments.actions.addComment(content);
+
+    toggleIsAddReply && toggleIsAddReply();
   };
 
   return (
@@ -21,7 +23,7 @@ function Add({ parent, user }) {
       className={`${addForm} ${parent ? reply : comment}`}
       onSubmit={handleSubmit}
     >
-      <UserImage user={user} />
+      <UserImage user={authedUser.state} />
       <textarea
         defaultValue={content}
         onChange={(e) => setContent(e.target.value)}
